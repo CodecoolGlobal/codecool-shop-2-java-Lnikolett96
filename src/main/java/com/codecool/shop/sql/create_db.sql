78cc3943bdeac37f7f0d32f06113a00c24920512
MiMi0001 <willijiem@gmail.com>
2023-01-10 15:54:52 +0100

    Created SQL script

---
+++ src/main/java/com/codecool/shop/sql/create_db.sql
@@ -0,0 +1,43 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR,
+    price NUMERIC,
+    category_id INTEGER,
+    supplier_id INTEGER,
+    image_file_name VARCHAR
+);
+
+CREATE TABLE suppliers (
+   id SERIAL PRIMARY KEY NOT NULL,
+   name VARCHAR
+);
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT fk_supplier_id FOREIGN KEY (supplier_id) REFERENCES suppliers(id);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR
+);
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories(id);
+
+CREATE TABLE order_header (
+    id SERIAL PRIMARY KEY NOT NULL,
+    order_date DATE,
+    order_total NUMERIC
+);
+
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY NOT NULL,
+    order_id INTEGER,
+    product_id INTEGER,
+    quantity INTEGER
+);
+
+ALTER TABLE ONLY order_items
+    ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES order_header(id);
+
+ALTER TABLE ONLY order_items
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products(id);
